e325333681d8167e4a517771534fb93e9a1fb3e2
Lucasstq <fit.luk@outlook.com>
2025-09-22 13:55:11 -0300

    feat: Coloca anotion @EnableMethodSecurity para que possa ser validado nossas rotas.

--- src/main/java/br/com/vaquejada_digital/VaquejadaDigital/VaquejadaDigitalApplication.java
+++ src/main/java/br/com/vaquejada_digital/VaquejadaDigital/VaquejadaDigitalApplication.java
@@ -2,8 +2,10 @@ package br.com.vaquejada_digital.VaquejadaDigital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true)
 public class VaquejadaDigitalApplication {
 
 	public static void main(String[] args) {
